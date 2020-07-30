6a2325e594622b4230d42318c0f2ada46ebded2f
ArtusU <artusrock@hotmail.com>
2020-07-30 17:23:55 +0100

    serializer

--- src/core/views.py
+++ src/core/views.py
@@ -5,21 +5,20 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
+from .serializers import PostSerializer
+from .models import Post
+
 class TestView(APIView):
     def get(self, request, *args, **kwargs):
-        data = {
-            'name': 'Mickey',
-            'age': 54
-        }
-        return Response(data)
-
-
-
-
-# Create your views here.
-# def test_view(request):
-#    data = {
-#        'name': 'Mickey',
-#        'age': 54
-#    }
-#    return JsonResponse(data)
+        qs = Post.objects.all()
+        #post = qs.first()
+        serializer = PostSerializer(qs, many=True)
+        #serializer = PostSerializer(post)
+        return Response(serializer.data)
+    
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
